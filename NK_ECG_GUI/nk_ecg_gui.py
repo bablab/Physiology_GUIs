f9c8516245bfbc1655df2c5d1cc86724c98f653f
psavoca <ps365@g.ucla.edu>
2023-06-30 16:13:16 -0400

    Initial Edits

--- NK_ECG_GUI/nk_ecg_gui.py
+++ NK_ECG_GUI/nk_ecg_gui.py
@@ -18,40 +18,86 @@ The application generates 2 output files:
 These files can then be used for ECG-based analyses (e.g., IBI, HRV, ect.)
 """
 
-#-----------------Start Updated Code
-
 #Load Dependencies
 import numpy as np
 from numpy import savetxt
 import neurokit2 as nk
 #import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from mpl_point_clicker import clicker
+#from mpl_point_clicker import clicker
+#import mpl_connect
 from matplotlib.figure import Figure
 import PySimpleGUI as sg
 import os
 
+
+#Create Empty Delete and Add Peak Arrays
+delete_peaks=[]
+add_peaks=[]
+
 #Define Plotting Functions
 def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
     figure_canvas_agg = FigureCanvasTkAgg(fig, master=canvas)
-    figure_canvas_agg.draw()
+    #figure_canvas_agg.draw()
     toolbar = Toolbar(figure_canvas_agg, canvas_toolbar)
-    toolbar.update()
+    #toolbar.update()
     figure_canvas_agg.get_tk_widget().pack(side='right', fill='both', expand=1)
     return figure_canvas_agg, toolbar
 
 def draw(figure_canvas_agg, toolbar):
     figure_canvas_agg.draw()
-    toolbar.update()
+    #toolbar.update()   
 
+def onpick1(event):
+    thisline = event.artist
+    xval = thisline.get_xdata()
+    yval = thisline.get_ydata()
+    ind = event.ind
+    
+    if edit_mode == 'add' and thisline == ecg:
+        max_ind = np.argmax(yval[ind])
+        max_ind = ind[max_ind]
+        local_max = findpeak(max_ind, ecg_clean)
+        ax.plot(xval[local_max], yval[local_max], 'yo')
+        draw(figure_canvas_agg, toolbar)
+        add_peaks.append(local_max)
+        
+    if edit_mode == 'remove' and thisline == points:
+        ax.plot(xval[ind], yval[ind], 'rx')
+        draw(figure_canvas_agg, toolbar)
+        delete_peaks.append(int(ind))       
+        
+#Snap to Peak Correction
+def findpeak(x, ecg_clean):
+    peak_error = SR * 0.125
+    win_start = int(x - peak_error)
+    win_end = int(x + peak_error)
+    if win_start < 0:
+        win_start = 0
+    if win_end > len(ecg_clean)*SR:
+        win_end = len(ecg_clean) *SR
+    x = np.argmax(ecg_clean[((win_start)):((win_end))]) + (x - peak_error)
+    return int(x)
+
+#Ploting Clean ECG with R-peaks
+def ecg_plot():
+    fig.subplots_adjust(right=.95, left=0.1, bottom=0.15, top=0.9)
+    time = np.linspace(0, ecg_clean.shape[0] / SR, ecg_clean.shape[0])
+    ax.set_title("ECG Data")
+    ax.set_xlabel("Time (Seconds)")
+    ax.set_ylabel("Volts")
+    ecg, = ax.plot(time, ecg_clean, 'r-', picker=True)
+    points, = ax.plot(peaks/SR, ecg_clean[peaks], "bo", picker=True) 
+    draw(figure_canvas_agg, toolbar)
+    return ecg, points
 
 class Toolbar(NavigationToolbar2Tk):
     def __init__(self, *args, **kwargs):
         super(Toolbar, self).__init__(*args, **kwargs)
 
-##Starting Screen
+#Starting Screen
 layout_intro = [[sg.T("")], [sg.Text("Information about ECG Cleaning Application.")],
-          [sg.Text("Intructions:")],
+          [sg.Text("Instructions:")],
           [sg.Text("1) Select .Acq Physio File")],
           [sg.Text("2) Select ECG Source Channel and Filtering Parameters")],
           [sg.Text("3) View Filtered Data -- Add or Remove R Peaks")],
@@ -61,43 +107,43 @@ layout_intro = [[sg.T("")], [sg.Text("Information about ECG Cleaning Application
 
 #Build Window
 window_intro = sg.Window('ECG Cleaning App', layout_intro, size=(600,400))
-    
+        
 while True:
     event, values = window_intro.read()
     if event == sg.WIN_CLOSED or event=="Exit":
+        exit()
         window_intro.close()
         break
+        
     elif event == "Continue":
         window_intro.close()
         break
-
-##Input Selection Screen
-layout_file = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],[sg.Button("Submit")]]
+        
+#Input Selection Screen
+layout_file = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],
+               [sg.Button("Load"), sg.Button("Exit")]]
 
 #Build Window
 window_file = sg.Window('ECG Cleaning App', layout_file, size=(600,150))
-
     
 while True:
     event, values = window_file.read()
     if event == sg.WIN_CLOSED or event=="Exit":
+        exit()
         break
-    elif event == "Submit":
+    elif event == "Load":
         file_name = values["-IN-"]
         window_file.close()
         break
-    
-      
-physio_file = nk.read_acqknowledge(file_name)
-physio_file = physio_file[0]
+       
+#Pull Channel names and Sampling Rate
+physio_file, sampling_rate = nk.read_acqknowledge(file_name)
 channels = tuple(physio_file.columns)
 
 ##Window to Select Output location & Sampling Rate  
-layout_settings = [[sg.T("")],[sg.Text("Select ECG Source:"), sg.Combo(channels, default_value='',key='-IN4-')],
-         [sg.Text("Sampling Rate (Hz):"), sg.Input(key='-IN3-')],
+layout_settings = [[sg.T("")],[sg.Text("Select ECG Source:"), sg.Combo(channels, default_value='',key='-Source-')],
          [sg.Text("Notch Filter:"),sg.Radio("60Hz", "notch", key='60', default=True),sg.Radio("50Hz", "notch", key='50')],
-         [sg.T("")], [sg.Text("Choose Output Location: "), sg.Input(), sg.FolderBrowse(key="-IN2-")],
-         [sg.Button("Submit")]]
+         [sg.Button("Plot Signal"), sg.Button("Exit")]]
 
 ###Building Window
 window_settings = sg.Window('ECG Cleaning App', layout_settings, size=(600,300))
@@ -105,11 +151,12 @@ window_settings = sg.Window('ECG Cleaning App', layout_settings, size=(600,300))
 while True:
     event, values = window_settings.read()
     if event == sg.WIN_CLOSED or event=="Exit":
+        exit()
         break
-    elif event == "Submit":
-        output_location = values["-IN2-"]
-        sampling_rate = int(values["-IN3-"])
-        ecg_source = values["-IN4-"]
+    elif event == "Plot Signal":
+        #output_location = values["-IN2-"]
+        #sampling_rate = int(values["-IN3-"])
+        ecg_source = values["-Source-"]
         if values['60']==True:
             notch_filter= 60
         elif values['50']==True:
@@ -122,19 +169,18 @@ Raw_ECG = physio_file[ecg_source]
 SR=sampling_rate
 ecg_clean = nk.ecg_clean(Raw_ECG, sampling_rate=SR, method='neurokit', powerline=notch_filter)
 r_peaks = nk.ecg_peaks(ecg_clean, sampling_rate=SR)
+peaks = (np.where(r_peaks[0] == 1)[0])
+peaks = peaks.tolist()
 
 # 2. create PySimpleGUI window, a fixed-size Frame with Canvas which expand in both x and y.
-
 layout = [
     [sg.T('Edit Physio Data')],
-    [sg.B('Plot'), sg.B('Save'),sg.B('Exit')],
+    [sg.B('Re-Plot'), sg.B('Save'),sg.B('Exit')],
     [sg.T('Controls:')],
-    [sg.Canvas(key='controls_cv')],
-    [sg.T('Figure:')],
+    [sg.B('Add Peak', button_color = ('white','black')), sg.B('Remove Peak', button_color = ('white','black')),sg.Canvas(key='controls_cv')],
     [sg.Column(
         layout=[
             [sg.Canvas(key='fig_cv',
-                       # it's important that you set this size
                        size=(1500, 750),
                        expand_x=True,
                        expand_y=True
@@ -145,91 +191,64 @@ layout = [
     )],
 ]
 
-window = sg.Window('Matplotlib', layout, finalize=True, resizable = True)
+window = sg.Window('ECG Editor', layout, finalize=True, resizable = True)
 
-# 3. Create a matplotlib canvas under sg.Canvas or sg.Graph
-fig = Figure(figsize=(20, 10), dpi=125)#, subplotpars = True)
+# 3. Create a plot of the ECG Data
+fig = Figure(figsize=(20, 10), dpi=125)
+fig.canvas.mpl_connect('pick_event', onpick1)
 ax = fig.add_subplot()
-figure_canvas_agg, toolbar = draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas) #ADDED
+figure_canvas_agg, toolbar = draw_figure_w_toolbar(window['fig_cv'].TKCanvas, fig, window['controls_cv'].TKCanvas)
+ecg, points = ecg_plot()
 
-# 4. initial for figure
-ax.set_title("ECG Data")
-ax.set_xlabel("Time (Seconds)")
-ax.set_ylabel("Volts")
-
-
-# 5. PySimpleGUI event loop
+# 4. GUI Events
 while True:
     event, values = window.read()
+   
     if event in (sg.WINDOW_CLOSED, 'Exit'):
         break
-    elif event == 'Plot':
-        ax.cla()                    # Clear axes first if required
-        fig.subplots_adjust(right=.80, left=0.1, bottom=0.15, top=0.9)
-        time = np.linspace(0, ecg_clean.shape[0] / SR, ecg_clean.shape[0])
-        peaks = (np.where(r_peaks[0] == 1)[0])
-        ax.set_title("ECG Data")
-        ax.set_xlabel("Time (Seconds)")
-        ax.set_ylabel("Volts")
-        ax.plot(time, ecg_clean, 'r-')
-        ax.plot(peaks/SR, ecg_clean[peaks], "o") 
-        klicker = clicker(ax, ["Remove", "Add"], markers=["X","o"])
-        draw(figure_canvas_agg, toolbar)             # do Update to GUI canvas
-
-#Saving Edits
-    elif event == 'Save':
-        #Error parameter for snap-to-peak funcationality. Error is +/- 0.125s
-        peak_error = SR * 0.125 
-        
-        ecg_edits = klicker.get_positions()
-        add = (ecg_edits['Add'])
-        add = [i[0] for i in add]
-        
-        add = [i * SR for i in add]
-        
-        #Snap to Peak Correction
-        for i in add:
-            add_start = int(i-peak_error)
-            add_end = int(i+peak_error)
-            
-            if add_start < 0:
-                add_start = 0
-                
-            if add_end > len(ecg_clean) * SR:
-                add_end = len(ecg_clean) * SR
-                
-            i = np.argmax(ecg_clean[((add_start)):((add_end))]) + (i-peak_error)
     
-        add = [int(i) for i in add]
-        remove = ecg_edits['Remove']
-        remove = [i[0] for i in remove]
-        remove = [i * SR for i in remove]
-        all_peaks = [*remove, *add, *peaks]
-        all_peaks = sorted(all_peaks)
+    elif event == 'Add Peak':
+        edit_mode = 'add'
+        window['Add Peak'].update(button_color = ('black','yellow'))
+        window['Remove Peak'].update(button_color = ('white','black'))
+        
     
-        i=1;
-        while i<len(all_peaks):
-            if(all_peaks[i]-all_peaks[i-1] < peak_error):
-                all_peaks.remove(all_peaks[i])
-                all_peaks.remove(all_peaks[i-1])
-            else:
-                i+=1
-        all_peaks = np.array(all_peaks)
-
-        ax.cla()                    # Clear axes first if required
-        fig.subplots_adjust(right=.80, left=0.1, bottom=0.15, top=0.9)
-        time = np.linspace(0, ecg_clean.shape[0] / SR, ecg_clean.shape[0])
-        peaks = (np.where(r_peaks[0] == 1)[0])
-        ax.set_title("ECG Data")
-        ax.set_xlabel("Time (Seconds)")
-        ax.set_ylabel("Volts")
-        
-        ax.plot(time, ecg_clean, 'r-')
-        ax.plot(all_peaks/SR, ecg_clean[all_peaks], "o")
-        
+    elif event == 'Remove Peak':
+        edit_mode = 'remove'
+        window['Remove Peak'].update(button_color = ('black','yellow'))
+        window['Add Peak'].update(button_color = ('white','black'))
+       
+    
+    elif event == 'Re-Plot':
+        #Reset Graph and Edited Points
+        ax.cla()
+        add_peaks =[]
+        delete_peaks = []
+        #New Plot
+        ecg, points = ecg_plot()
      
-        draw(figure_canvas_agg, toolbar)             # do Update to GUI canvas
+#Saving Edits & Plot Final Product
+    elif event == 'Save':
+
+        #Open pop-up to select output folder
+        output_location=sg.popup_get_folder('Select Output Location', title="Output Folder")
+
+        #Clear what was previously drawn
+        ax.cla()                  
         
+        #Remove Peaks Identified as Errors
+        for i in sorted(delete_peaks, reverse=True):
+            del peaks[int(i)]
+        
+        #Add Peaks identified as missing
+        for i in add_peaks:
+            peaks.append(i)  
+        peaks = np.array(sorted(peaks))
+        
+        #Plot Final Product -- No more ability to edit; gray out buttons
+        window['Remove Peak'].update(button_color = ('white','gray'), disabled=True)
+        window['Add Peak'].update(button_color = ('white','gray'), disabled=True)
+        ecg_plot()
         
         #Save Outputs
         ppt_name = file_name.rsplit('/',1)[1]
@@ -237,10 +256,10 @@ while True:
         Rs_name = "{}_r_peaks_edited.csv".format(ppt_name)
         ECG_name = "{}_ECG_cleaned.csv".format(ppt_name)
         os.chdir(output_location)
-        savetxt(Rs_name, all_peaks, delimiter=',')
+        savetxt(Rs_name, peaks, delimiter=',')
         savetxt(ECG_name, ecg_clean, delimiter=',')
 
-# 7. Close window to exit
+#Close window to exit
 window.close()
 
 
